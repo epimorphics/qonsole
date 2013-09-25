afb98cf3820770f9400b4728800dffbdabee9907
Ian Dickinson <i.j.dickinson@gmail.com>
2013-09-25 13:05:02 +0100

    Restore basic query capability

--- js/app/qonsole.js
+++ js/app/qonsole.js
@@ -3,9 +3,10 @@ var qonsole = function() {
 
   /** The loaded configuration */
   var _config = {};
-  var _defaults = {};
-  var _current_query = null;
+  // var _defaults = {};
+  // var _current_query = null;
   var _query_editor = null;
+  var _startTime = 0;
 
   /** Failed to load the configuration */
   var onConfigFail = function() {
@@ -18,130 +19,6 @@ var qonsole = function() {
     var uri = elt.attr("value");
 
     addPrefixDeclaration( prefix, uri, elt.is( ":checked" ) );
-  };
-
-  var endpointURL = function() {
-    var ep = null;
-    if (currentQueryConfig()) {
-      ep = currentQueryConfig().endpoint;
-    }
-
-    ep = ep || (_defaults && _defaults.endpoint);
-    ep = ep || "/sparql";
-
-    return ep;
-  };
-
-  var selectedFormat = function() {
-    return $("#format-choice button.active" ).attr( "data-format" );
-  };
-
-  var onQueryFail = function( jqXHR, textStatus, errorThrown, then ) {
-    showPlainTextResult( jqXHR.responseText || jqXHR.statusText, then, 0, "errorText", null );
-  };
-
-  var onQuerySuccess = function( data, then ) {
-    switch (selectedFormat()) {
-      case "text":
-        showPlainTextResult( data, then, null, null, "errorText" );
-        break;
-      case "json":
-        showPlainTextResult( data, then, "errorText" );
-        break;
-      case "xml":
-        showPlainTextResult( data, then, "errorText" );
-        break;
-      case "tsv":
-        showTableResult( data, then );
-        break;
-    }
-  };
-
-  var clearResults = function() {
-    $("#results").empty();
-    $("#loadingSpinner").hide();
-    $("#timeTaken").addClass( "hidden" );
-  };
-
-  var showPlainTextResult = function( data, then, count, addClass, removeClass ) {
-    var lineLength = 100;
-    var lineLength = 0;
-    var lineCount = 0;
-    var counting = true;
-    var k = 0;
-
-    while (counting) {
-      var nextNewline = data.indexOf( "\n", k );
-      if (nextNewline < 0) {
-        counting = false;
-      }
-      else {
-        lineCount++;
-
-        if ((nextNewline - k) > lineLength) {
-          lineLength = nextNewline - k;
-        }
-
-        k = nextNewline + 1;
-      }
-    }
-
-    // we ignore 4 rows of chrome in the query format
-    showTimeTaken( (count === null) ? lineCount - 4 : count, then );
-
-
-    // versions of IE break the text formatting of pre nodes. Why? FFS
-    if (isIE()) {
-      $("#results").html( "<div></div>" );
-      var n = $("#results").children().get( 0 );
-      n.outerHTML = sprintf( "<pre class='span12 results-plain' style='min-width: %dpx'>", lineLength * 8 ) + data.replace( /</g, "&lt;" ) + "</pre>";
-    }
-    else {
-      $( "#results" ).html( sprintf( "<pre class='span12 results-plain' style='min-width: %dpx'></pre>", lineLength * 8 ));
-      $( "#results pre.results-plain" ).text( data );
-    }
-    $( "#results pre.results-plain" ).addClass( addClass )
-                                     .removeClass( removeClass );
-  };
-
-  var showTableResult = function( data, then ) {
-    var lines = data.split( "\n" );
-    var lineCount = 0;
-
-    var data = new google.visualization.DataTable();
-    $.each( lines.shift().split("\t"), function(i, c ) {data.addColumn('string', c);} );
-
-    $.each( lines, function( i, l ) {
-      if (l && l !== "") {
-        lineCount++;
-        var d = [];
-        $.each( l.split( "\t" ), function( i, v ) {d.push( /*v.slice( 1, -1 )*/ v );} ); // TODO format values properly
-        data.addRows( [d] );
-      }
-    } );
-
-    showTimeTaken( lineCount, then );
-    $("#results").empty();
-    var table = new google.visualization.Table(document.getElementById('results'));
-    table.draw(data, {
-      showRowNumber: true,
-      page: "enable",
-      pageSize: 25,
-      alternatingRowStyle: true
-    });
-
-  };
-
-  var showTimeTaken = function( count, then ) {
-    var duration = new Date().getTime() - then.getTime();
-    var ms = duration % 1000;
-    duration = Math.floor( duration / 1000 );
-    var s = duration % 60;
-    var m = Math.floor( duration / 60 );
-
-    var html = (count === 1) ? "1 row in: " : (count + " rows in: ");
-    $("#timeTaken").html( html + m + "min " + s + "s " + ms + "ms." );
-    $("#timeTaken").removeClass("hidden");
   };
 
   var isOpera = function() {return !!(window.opera && window.opera.version);};  // Opera 8.0+
@@ -164,10 +41,14 @@ var qonsole = function() {
     // $("#prefixes").click( "input", addOrRemovePrefix );
     // $("#query-chrome2 a").click( runQuery );
 
-    $('#loadingSpinner')
-      .hide()
-      .ajaxStart(function() {$(this).show();})
-      .ajaxStop(function() {$(this).hide();});
+    $(document)
+      .ajaxStart(function() {
+        elementVisible( ".loadingSpinner", true );
+        startTimingResults();
+      })
+      .ajaxStop(function() {
+        elementVisible( ".loadingSpinner", false );
+      });
 
     loadConfig( config );
     bindEvents();
@@ -213,6 +94,7 @@ var qonsole = function() {
       var elem = $(e.currentTarget);
       setCurrentEndpoint( elem.text().trim() );
     } );
+    $("a.run-query").on( "click", runQuery );
   };
 
   /** List the current defined prefixes from the config */
@@ -259,7 +141,7 @@ var qonsole = function() {
 
   /** Return the current endpoint text */
   var currentEndpoint = function( url ) {
-    $("[id=sparqlEndpoint]").val();
+    return $("[id=sparqlEndpoint]").val();
   };
 
   /** Set the initial query, which will be the default selection plus the selected prefixes */
@@ -305,6 +187,11 @@ var qonsole = function() {
     setCurrentQueryText( q );
   };
 
+  /** Return the currenty selected output format */
+  var selectedFormat = function() {
+    return $("#displayFormat").val();
+  };
+
   /** Return a string comprising the currently selected prefixes */
   var renderCurrentPrefixes = function() {
     var l = $("ul.prefixes a.active" ).map( function( i, elt ) {
@@ -339,46 +226,154 @@ var qonsole = function() {
     setCurrentQueryText( lines.join( "\n" ) );
   };
 
-  /** Show or hide the timeTaken field */
-  var setTimeTakenVisibility = function( visible ) {
-    if (visible) {
-      $("#timeTaken").removeClass( "hidden" );
-    }
-    else {
-      $("#timeTaken").addClass( "hidden" );
-    }
-  };
-
   /** Perform the query */
   var runQuery = function( e ) {
     e.preventDefault();
-    setTimeTakenVisibility( false );
 
     var url = currentEndpoint();
     var query = currentQueryText();
     var format = selectedFormat();
-    var now = new Date();
 
     var options = {
       data: {query: query, output: format},
-      success: function( data ) {onQuerySuccess( data, now );},
-      error: function( x, t, e ) {onQueryFail( x, t, e, now );}
+      success: function( data, xhr ) {
+        onQuerySuccess( data, format );
+      },
+      error: onQueryFail
     };
 
-    // hack TODO remove
-    if (selectedFormat() === "xml" || selectedFormat() === "json"){
-      options.data["force-accept"] = "text/plain";
-    }
-
-    // IE doesn't speak CORS
-    if (isIE()) {
-      // options.dataType = "jsonp";
-    }
+    checkForceTextFormat( format, options );
+    checkForceJsonP( options );
 
     $.ajax( url, options );
   };
 
+  /** Hide or reveal an element using Bootstrap .hidden class */
+  var elementVisible = function( elem, visible ) {
+    if (visible) {
+      $(elem).removeClass( "hidden" );
+    }
+    else {
+      $(elem).addClass( "hidden" );
+    }
+  };
 
+  /** Prepare to show query time taken */
+  var startTimingResults = function() {
+    _startTime = new Date().getTime();
+    elementVisible( ".timeTaken" );
+  };
+
+  /** Show results count and time */
+  var showResultsTimeAndCount = function( count ) {
+    var duration = new Date().getTime() - _startTime;
+    var ms = duration % 1000;
+    duration = Math.floor( duration / 1000 );
+    var s = duration % 60;
+    var m = Math.floor( duration / 60 );
+    var suffix = (count !== 1) ? "s" : "";
+
+    var html = sprintf( "%s result%s in %d min %d.%03d s", count, suffix, m, s, ms );
+
+    $(".timeTaken").html( html );
+    elementVisible( ".timeTaken", true );
+  };
+
+  /** For display purposes, we want the browser to not parse some formats for us */
+  var checkForceTextFormat = function( format, options ) {
+    if (format === "xml" || format === "json"){
+      options.data["force-accept"] = "text/plain";
+    }
+  };
+
+  /** Can we use CORS, or do we need to force the use of JsonP? */
+  var checkForceJsonP = function( options ) {
+    if (isIE()) {
+      // options.dataType = "jsonp";
+    }
+  };
+
+  /** Report query failure */
+  var onQueryFail = function( jqXHR, textStatus, errorThrown ) {
+    showPlainTextResult( jqXHR.responseText || jqXHR.statusText, 0, "errorText", null );
+  };
+
+  /** Query succeeded - use display type to determine how to render */
+  var onQuerySuccess = function( data, format ) {
+    switch (format) {
+      case "text":
+        showPlainTextResult( data, null, null, "errorText" );
+        break;
+      case "json":
+        showPlainTextResult( data, "errorText" );
+        break;
+      case "xml":
+        showPlainTextResult( data, "errorText" );
+        break;
+      case "tsv":
+        showTableResult( data );
+        break;
+    }
+  };
+
+  var showPlainTextResult = function( data, count, addClass, removeClass ) {
+    var lineLength = 0;
+    var lineCount = 0;
+    var nextNL = 0;
+
+    while (nextNL >= 0) {
+      var k = nextNL;
+      nextNL = data.indexOf( "\n", k + 1 );
+      if (nextNL > 0) {
+        lineCount++;
+        lineLength = _.max( [nextNL - k, lineLength] );
+      }
+    }
+
+    // we ignore 4 rows of chrome in the query format
+    showResultsTimeAndCount( (count === null) ? lineCount - 4 : count );
+
+    // versions of IE break the text formatting of pre nodes. Why? FFS
+    if (isIE()) {
+      $("#results").html( "<div></div>" );
+      var n = $("#results").children().get( 0 );
+      n.outerHTML = sprintf( "<pre class='span12 results-plain' style='min-width: %dpx'>", lineLength * 8 ) + data.replace( /</g, "&lt;" ) + "</pre>";
+    }
+    else {
+      $( "#results" ).html( sprintf( "<pre class='span12 results-plain' style='min-width: %dpx'></pre>", lineLength * 8 ));
+      $( "#results pre.results-plain" ).text( data );
+    }
+    $( "#results pre.results-plain" ).addClass( addClass )
+                                     .removeClass( removeClass );
+  };
+
+  var showTableResult = function( data, then ) {
+    var lines = data.split( "\n" );
+    var lineCount = 0;
+
+    var data = new google.visualization.DataTable();
+    $.each( lines.shift().split("\t"), function(i, c ) {data.addColumn('string', c);} );
+
+    $.each( lines, function( i, l ) {
+      if (l && l !== "") {
+        lineCount++;
+        var d = [];
+        $.each( l.split( "\t" ), function( i, v ) {d.push( /*v.slice( 1, -1 )*/ v );} ); // TODO format values properly
+        data.addRows( [d] );
+      }
+    } );
+
+    // showTimeTaken( lineCount );
+    $("#results").empty();
+    var table = new google.visualization.Table(document.getElementById('results'));
+    table.draw(data, {
+      showRowNumber: true,
+      page: "enable",
+      pageSize: 25,
+      alternatingRowStyle: true
+    });
+
+  };
 
   return {
     init: init
