2887ab9369eb8b089427a1fd39123be4f6e47538
Ian Dickinson <i.j.dickinson@gmail.com>
2015-07-16 23:46:05 +0100

    Rationalise grunt install-dependencies task. Library files are only copied from bower_components, and all go under ./lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,50 +7,62 @@ module.exports = function(grunt) {
     bower: {
       install: {}
     },
+    clean: {
+      lib: ["lib"]
+    },
     copy: {
       dependencies: {
         files: [
           {
             expand: true,
             flatten: true,
-            src: ['lib/@(bootstrap|CodeMirror|datatables|jquery|jquery.xdomainrequest|sprintf|underscore)/*.js',
-                  'lib/html5shiv/dist/html5shiv.js',
-                  'lib/qonsole/*.js',
-                  'lib/qonsole/js/app/*.js',
-                  'bower_components/respond/dest/respond.min.js',
+            src: ['bower_components/respond/dest/respond.min.js',
                   'bower_components/CodeMirror/mode/@(javascript|sparql|xml)/*.js',
-                  'bower_components/CodeMirror/addon/fold/*.js'
-                 ], 
-            dest: 'js/lib/'
+                  'bower_components/CodeMirror/addon/fold/*.js',
+                  "bower_components/bootstrap/dist/js/bootstrap.js",
+                  "bower_components/jquery/jquery.js",
+                  "bower_components/datatables/media/js/jquery.dataTables.js",
+                  "bower_components/html5shiv/dist/html5shiv.js",
+                  "bower_components/jquery.xdomainrequest/jQuery.XDomainRequest.js",
+                  "bower_components/lodash-compat/lodash.js",
+                  "bower_components/spin/javascripts/jquery.spin.js",
+                  "bower_components/sprintf/dist/sprintf.min.js"
+                 ],
+            dest: 'lib/js'
           },
           {
             expand: true,
             flatten: true,
-            src: ['lib/@(bootstrap|CodeMirror|datatables|components-font-awesome)/*.css',
-                  'lib/qonsole/css/*.css'
-                 ], 
-            dest: 'css'
+            src: ["bower_components/CodeMirror/addon/fold/*.css",
+                  "bower_components/bootstrap/dist/css/bootstrap.css",
+                  "bower_components/datatables/media/css/jquery.dataTables.css",
+                  "bower_components/fontawesome/css/font-awesome.css",
+                  "bower_components/spin/stylesheets/jquery.spin.css"
+                 ],
+            dest: 'lib/css'
           },
           {
             expand: true,
             flatten: true,
-            src: ['lib/bootstrap/*.@(eot|svg|ttf|woff)'], 
-            dest: 'font/'
+            src: ["bower_components/fontawesome/fonts/*"
+            ],
+            dest: 'lib/fonts'
           },
           {
             expand: true,
             flatten: true,
-            src: ['lib/datatables/*.png'],
-            dest: 'images'
+            src: ['bower_components/datatables/media/images/*.png'],
+            dest: 'lib/images'
           }
         ]
       }
     }
   });
-  
+
   grunt.loadNpmTasks('grunt-bower-task');
-  grunt.loadNpmTasks('grunt-contrib-copy');  
-  
-  grunt.registerTask('install-dependencies', ['bower:install', 'copy:dependencies'] );
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('install-dependencies', ['bower:install', "clean:lib", 'copy:dependencies'] );
 
 };
