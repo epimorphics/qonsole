f16ab11ca9965b6546627d80df6015a8b16acd5f
Mike Waterworth <mike.waterworth@epimorphics.com>
2018-08-16 16:42:16 +0100

    Move header code in to own template

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="brand">Qonsole</h1>
+    <h2 class="brand">a query console for SPARQL</h2>
+  </div>
+</template>
+<style scoped>
+  h1, h2 {
+    float: left;
+    margin-right: 0.5em;
+  }
+</style>
