c7c46e083041cc6a4f218016df7a9089b26d62c4
Mike Waterworth <mike.waterworth@epimorphics.com>
2018-08-22 13:12:12 +0100

    Add Element UI elements

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import VueHighlightJS from 'vue-highlightjs'
 import VueMoment from 'vue-moment'
+import './plugins/element.js'
 Vue.config.productionTip = false
 
 Vue.use(VueMoment)
